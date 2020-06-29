0a2aa1c28374b7a411e8079dceb4134764be9e0e
gyks <gyksqop@gmail.com>
2020-06-29 17:29:43 +0300

    checkpoint 3

---
+++ src/components/app/app.js
@@ -0,0 +1,25 @@
+import React from "react";
+import TodoHeader from "../todo-header";
+import TodoList from "../todo-list";
+import SearchPanel from "../search-panel";
+import ItemStatusFilter from "../item-status-filter";
+
+const App = () => {
+  const listItems = [
+    { label: "Drink a tone of Coffee", important: false, id: 1 },
+    { label: "Walk with your dog", important: true, id: 2 },
+    { label: "Get a Real Job", important: true, id: 3 },
+  ];
+  return (
+    <div>
+      <TodoHeader />
+      <div className="input-group mb-1">
+        <SearchPanel />
+        <ItemStatusFilter />
+      </div>
+      <TodoList listItems={listItems} />
+    </div>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import TodoHeader from "./components/todo-header";
-import TodoList from "./components/todo-list";
-import SearchPanel from "./components/search-panel";
-import ItemStatusFilter from "./components/item-status-filter";
-
-const App = () => {
-  const listItems = [
-    { label: "Drink a tone of Coffee", important: false, id: 1 },
-    { label: "Walk with your dog", important: true, id: 2 },
-    { label: "Get a Real Job", important: true, id: 3 },
-  ];
-  return (
-    <div>
-      <TodoHeader />
-      <div className="input-group mb-1">
-        <SearchPanel />
-        <ItemStatusFilter />
-      </div>
-      <TodoList listItems={listItems} />
-    </div>
-  );
-};
+import App from "./components/app";
 
 ReactDOM.render(<App />, document.getElementById("root"));
